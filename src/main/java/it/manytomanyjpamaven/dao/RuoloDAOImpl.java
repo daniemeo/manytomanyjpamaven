ffbbabdd925a7418cede1946a3c261d0e9f6bb75
daniemeo <37437362+daniemeo@users.noreply.github.com>
2020-11-14 23:56:45 +0100

    primo commit manytomany

---
+++ src/main/java/it/manytomanyjpamaven/dao/RuoloDAOImpl.java
@@ -0,0 +1,74 @@
+package it.manytomanyjpamaven.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import it.manytomanyjpamaven.model.Ruolo;
+import it.manytomanyjpamaven.model.Utente;
+
+public class RuoloDAOImpl implements RuoloDAO {
+
+	private EntityManager entityManager;
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	public List<Ruolo> list() throws Exception {
+		return entityManager.createQuery("from Ruolo",Ruolo.class).getResultList();
+	}
+
+	@Override
+	public Ruolo get(Long id) throws Exception {
+		return entityManager.find(Ruolo.class, id);
+	}
+
+	@Override
+	public void update(Ruolo ruoloInstance) throws Exception {
+		if (ruoloInstance == null) {
+			throw new Exception("Problema valore in input");
+		}
+		ruoloInstance = entityManager.merge(ruoloInstance);
+
+	}
+
+	@Override
+	public void insert(Ruolo ruoloInstance) throws Exception {
+		if (ruoloInstance == null) {
+			throw new Exception("Problema valore in input");
+		}
+		entityManager.persist(ruoloInstance);
+     }
+
+	@Override
+	public void delete(Ruolo ruoloInstance) throws Exception {
+		if (ruoloInstance == null) {
+			throw new Exception("Problema valore in input");
+		}
+		TypedQuery<Utente> query = entityManager.createQuery("select u FROM Utente u join u.ruoli r where r = :ruolo",Utente.class);
+		query.setParameter("ruolo", ruoloInstance);
+		
+		//se lista vuota, possiamo effettuare l'eliminazione
+		if(query.getResultList().isEmpty()) {
+		entityManager.remove(entityManager.merge(ruoloInstance));
+		} else {
+			System.out.println("Non si può eliminare il ruolo!"); 
+			
+		}
+
+	}
+
+	@Override
+	public Ruolo findByDescrizioneAndCodice(String descrizione, String codice) throws Exception {
+		TypedQuery<Ruolo> query = entityManager
+				.createQuery("select r from Ruolo r where r.descrizione=?1 and r.codice=?2", Ruolo.class)
+				.setParameter(1, descrizione)
+				.setParameter(2, codice);
+		
+		return query.getResultStream().findFirst().orElse(null);
+	}
+
+}
